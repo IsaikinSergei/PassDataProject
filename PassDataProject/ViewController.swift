edc5a284c4f243ca55fbeff32a9a65a9ce0ade1f
IsaikinSergei <akinfest007@gmail.com>
2020-12-08 18:23:47 +0400

    Initial Commit

---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Sergei Isaikin on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
